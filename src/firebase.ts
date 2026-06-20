d10b12c5bf71df367ea04ccb073e14b016594e65
moctafa <moctafa@gmail.com>
2026-06-20 05:56:17 +0300

    Fix firebase export

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCPlYvkqft-C1D_wt2UG-Slia4m0m-X8fs",
@@ -8,3 +11,6 @@ const firebaseConfig = {
   appId: "1:666349275836:web:7233d83628c0ab17861eb9",
   measurementId: "G-P4FC6KVJ5N"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
